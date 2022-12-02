cea0d16d0bb536c34756421e5f5f9906e5b2461c
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2022-12-02 12:43:04 +0330

    Add part 2, there was a part 2 lol

--- day-01/index.js
+++ day-01/index.js
@@ -1,3 +1,19 @@
 // Run in browser console on the input page
 // https://adventofcode.com/2022/day/1/input
-document.querySelector('pre').innerText.split('\n\n').map(elf => elf.split('\n').reduce((tmp, weight) => tmp + parseInt(weight), 0)).reduce((max, weight) => weight > max ? weight : max, -1)
+const part1 = document
+  .querySelector("pre")
+  .innerText.split("\n\n")
+  .map((elf) =>
+    elf.split("\n").reduce((tmp, weight) => tmp + parseInt(weight), 0)
+  )
+  .reduce((max, weight) => (weight > max ? weight : max), -1);
+
+const part2 = document
+  .querySelector("pre")
+  .innerText.split("\n\n")
+  .map((elf) =>
+    elf.split("\n").reduce((tmp, weight) => tmp + parseInt(weight), 0)
+  )
+  .sort((a, b) => (a > b ? -1 : 1))
+  .slice(0, 3)
+  .reduce((tmp, total) => tmp + total, 0);
